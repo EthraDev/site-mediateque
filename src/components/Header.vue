d9437334dae0ffd0188c7c22cda80ef9b7557dfb
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2023-02-22 22:22:44 +0100

    :tada: Init Project & Start Header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="bg-white bg-opacity-50 flex flex-row justify-between w-11/12 mx-auto text-lg h-[6vh] items-center">
+    <div class="menu flex flex-row w-3/12 justify-between bg-red-500">
+      <a href="#">Home</a>
+      <a href="#">Abonnements</a>
+      <a href="#">Témoignages</a>
+      <a href="#">About Us</a>
+    </div>
+    <div class="title flex flex-row justify-center bg-blue-500 w-1/3">
+      <h1>Paris Médiathèque</h1>
+    </div>
+    <div class="others flex flex-row justify-between bg-green-500 w-1/12">
+      <a href="#">Faq</a>
+      <a href="#">Contact</a>
+      <a href="#"><ion-icon name="person"></ion-icon></a>
+    </div>
+  </header>
+</template>
+
+<script>
+
+  export default {
+    name : 'HeaderView',
+  }
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-black h-[100vh]">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+
+  name: 'HomeView',
+
+  components: {
+    Header,
+  }
+}
+</script>
